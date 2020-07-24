d249e45103f98bb932037146c1865faee1a3eac0
Avinash Pancham <avinashpancham@outlook.com>
2020-07-25 00:06:03 +0200

    Further update weather bot code

--- dags/helpers/helpers_weather_bot.py
+++ dags/helpers/helpers_weather_bot.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 
 
@@ -6,55 +8,95 @@ def kelvin_to_celsius(temperature: float) -> float:
 
 
 def get_first_day(df: pd.DataFrame) -> pd.DataFrame:
-    return df.sort_values(by='dt').head(24).assign(hour= lambda df: pd.to_datetime(df.dt, unit='s').dt.hour)
+    return (
+        df.sort_values(by="dt")
+        .head(24)
+        .assign(hour=lambda df: pd.to_datetime(df.dt, unit="s").dt.hour)
+    )
 
 
-def expand_weather(df: pd.DataFrame) -> pd.DataFrame:
+def expand_weather_column(df: pd.DataFrame) -> pd.DataFrame:
     df_weather = pd.DataFrame(df.weather.str[0].tolist())
     return df.join(df_weather)
 
 
 def get_weather_metrics(df: pd.DataFrame) -> (float, float, str):
-    return kelvin_to_celsius(df.temp.mean()), kelvin_to_celsius(df.feels_like.mean()), 'Yes' if df.rain.notnull().any() else 'No'
+    return (
+        kelvin_to_celsius(df.temp.mean()),
+        kelvin_to_celsius(df.feels_like.mean()),
+        "Yes" if df.rain.notnull().any() else "No",
+    )
 
 
 def get_weather_description(df: pd.DataFrame) -> str:
     first_hour = df.iloc[0].hour
-    df_description = df.assign(end_hour=lambda df: df.hour + 1,
-                                        next_main=lambda df: df.main.shift(-1)) \
-        .query('main!=next_main') \
-        .assign(start_hour=lambda df: (df.hour.shift(1) + 1).fillna(first_hour).astype(int)) \
+    df_description = (
+        df.assign(
+            end_hour=lambda df: df.hour + 1, next_main=lambda df: df.main.shift(-1)
+        )
+        .query("main!=next_main")
+        .assign(
+            start_hour=lambda df: (df.hour.shift(1) + 1).fillna(first_hour).astype(int)
+        )
         .astype(str)
-    
-    return '\n'.join('From ' + df_description['start_hour'] + ' till ' + df_description['end_hour'] + ': ' + df_description.main)
+    )
+
+    return "\n".join(
+        "From "
+        + df_description["start_hour"]
+        + " till "
+        + df_description["end_hour"]
+        + ": "
+        + df_description.main
+    )
 
 
-def create_weather_message(temperature: float, apparent_temperature: float, rain: str, weather_description: str) -> str:
+def create_weather_message(
+    temperature: float, apparent_temperature: float, rain: str, description: str
+) -> str:
     return f"""
 Goodmorning,
 
 Todays weather forecast for Den Haag is:
-Temperature: {temperature:.1f}
-Apparent temperature: {apparent_temperature:.1f}
-Rain: {rain}
-Weather:
-{weather_description}
+Average temperature: {temperature:.1f} °C
+Average apparent temperature: {apparent_temperature:.1f} °C
+Potential rain: {rain}
+Weather forecast:
+{description}
 """
 
 
-def parse_response(**context):
-    response = context['task_instance'].xcom_pull(task_ids='get_weather')
+def parse_weather_response(**context) -> str:
+    response = json.loads(context["task_instance"].xcom_pull(task_ids="get_weather"))
+    redundant_columns = [
+        "dt",
+        "pressure",
+        "humidity",
+        "dew_point",
+        "clouds",
+        "visibility",
+        "wind_speed",
+        "wind_deg",
+        "pop",
+        "id",
+        "description",
+        "icon",
+        "weather",
+    ]
 
-    redundant_columns = ['dt', 'pressure', 'humidity', 'dew_point', 'clouds', 'visibility',
-                         'wind_speed', 'wind_deg', 'pop', 'id', 'description',
-                         'icon', 'weather']
-
-    df_forecast = pd.DataFrame(response['hourly']).pipe(get_first_day).pipe(expand_weather).drop(columns=redundant_columns)
+    df_forecast = (
+        pd.DataFrame(response["hourly"])
+        .pipe(get_first_day)
+        .pipe(expand_weather_column)
+        .drop(columns=redundant_columns)
+    )
 
     temperature, apparent_temperature, rain = get_weather_metrics(df=df_forecast)
     weather_description = get_weather_description(df=df_forecast)
 
-    return create_weather_message(temperature=temperature, apparent_temperature=apparent_temperature,
-                                 rain=rain, weather_description=weather_description)
-
-
+    return create_weather_message(
+        temperature=temperature,
+        apparent_temperature=apparent_temperature,
+        rain=rain,
+        description=weather_description,
+    )
--- dags/weather_bot.py
+++ dags/weather_bot.py
@@ -5,41 +5,37 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.http_operator import SimpleHttpOperator
 
+from helpers.helpers_weather_bot import parse_weather_response
+
 
 default_args = {
-    'owner': 'me',
-    'start_date': dt.datetime(2020, 7, 20, 0, 00, 00),
-    'concurrency': 1,
-    'retries': 0
+    "owner": "me",
+    "start_date": dt.datetime(2020, 7, 20, 0, 00, 00),
+    "concurrency": 1,
+    "retries": 0,
 }
 
 
-
-def parse_response(**context):
-    response = context['task_instance'].xcom_pull(task_ids='get_weather')
-    return
-
-
-with DAG('weather_bot',
-         catchup=False,
-         default_args=default_args,
-         schedule_interval='@daily',
-         ) as dag:
+with DAG(
+    "weather_bot", catchup=False, default_args=default_args, schedule_interval="@daily",
+) as dag:
 
     get_weather = SimpleHttpOperator(
-        task_id='get_weather',
-        method='POST',
-        http_conn_id='weather',
-        endpoint='data/2.5/onecall?lat=52.066669&lon=4.3&exclude=current,daily',
+        task_id="get_weather",
+        method="POST",
+        http_conn_id="weather",
+        endpoint="data/2.5/onecall?lat=52.066669&lon=4.3&exclude=current,daily",
         headers={"x-api-key": os.environ["OPEN_WEATHER_API_KEY"]},
         xcom_push=True,
         response_check=lambda response: response.ok,
-        dag=dag
+        dag=dag,
     )
 
-    process_response = PythonOperator(task_id='process_response',
-                                python_callable=parse_response,
-                                provide_context=True)
+    parse_response = PythonOperator(
+        task_id="parse_response",
+        python_callable=parse_weather_response,
+        provide_context=True,
+        do_xcom_push=True,
+    )
 
-get_weather >> process_response
-
+get_weather >> parse_response
